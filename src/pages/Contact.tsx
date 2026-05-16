6e660aa0c9defc511047914a7c2dc8db359132a6
Moses Kinuka <kinuka__@hotmail.com>
2026-05-16 21:18:41 +0200

    fixed contact form on mobile, added preconnect to font

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -36,12 +36,12 @@ const sendEmail = (e: React.FormEvent) => {
   };
 
     return <>
-        <div id="contact-container" className="w-[70vw] flex items-center mb-[20vh]">
-        <div className="w-[70vw] flex flex-row max-[1200px]:flex-col max-[1200px]:items-center text-lg p-4 mb-20 gap-8">
-            <div className="w-[30vw] max-[1200px]:w-[60vw] mr-8 max-[1200px]:mr-0 max-[1200px]:mb-20 flex items-center flex-col">
-                <img src={picture} className="m-8 w-[15vw] max-[1200px]:w-[30vw]  rounded-xl" alt="Picture of me" />
+        <div id="contact-container" className="w-[90vw] flex items-center mb-[20vh]">
+        <div className="w-full flex flex-row max-[1200px]:flex-col max-[1200px]:items-center text-lg p-4 mb-20 gap-8">
+            <div className="w-full max-w-[44vw] max-[1200px]:w-[90vw] max-[1200px]:max-w-none max-[1200px]:mb-20 flex items-center flex-col">
+                <img src={picture} className="m-8 w-[22.5vw] max-[1200px]:w-[45vw] rounded-xl" alt="Picture of me" />
                 <h3 className="text-start text-white font-bold ">{type === "sv" ? "Kontakt" : "Contact"}</h3>
-                <p className="text-left text-white"> {type === "sv" ? "Har du frågor, idéer eller funderingar? Tveka inte att höra av dig! Jag svarar gärna på allt som rör mina projekt, samarbeten eller frontend-utveckling i allmänhet. Fyll i formuläret så hör jag av mig så snart jag kan." 
+                <p className="text-left text-white w-full"> {type === "sv" ? "Har du frågor, idéer eller funderingar? Tveka inte att höra av dig! Jag svarar gärna på allt som rör mina projekt, samarbeten eller frontend-utveckling i allmänhet. Fyll i formuläret så hör jag av mig så snart jag kan." 
                 : "Do you have any questions, ideas, or thoughts? Don't hesitate to get in touch! I'll be happy to answer anything related to my projects, collaborations, or frontend development in general. Fill out the form and I'll get back to you as soon as I can."}
                 </p>
                 <div className="flex flex-row justify-center gap-4 max-[1200px]:gap-8 w-full mt-6">
@@ -56,7 +56,7 @@ const sendEmail = (e: React.FormEvent) => {
                     </a>
                 </div>
             </div>
-            <div className="w-[30vw] max-[1200px]:w-[60vw] h-fit justify-center items-center rounded-2xl px-8 py-7 shadow-[0_4px_30px_rgba(0,0,0,0.8)] backdrop-blur-sm border border-white/30">
+            <div className="w-full max-w-[44vw] max-[1200px]:w-[90vw] max-[1200px]:max-w-none h-fit justify-center items-center rounded-2xl px-8 py-7 shadow-[0_4px_30px_rgba(0,0,0,0.8)] backdrop-blur-sm border border-white/30">
             {/* <h2>Har du frågor eller funderingar</h2> */}
                 <form className="flex flex-col w-full" ref={form} onSubmit={sendEmail}>
                     <h2 className="font-semibold text-[2rem] text-white mb-6 text-left">{type === "sv" ? "Kontakta mig" : "Contact me"}</h2>
